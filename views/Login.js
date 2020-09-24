7da537a88e4cb6ad34b2b0cdcbe255845faae963
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-09-23 21:09:51 -0600

    Creating the formLogin

--- views/Login.js
+++ views/Login.js
@@ -1,8 +1,35 @@
 import React from 'react';
-import {Text} from 'react-native';
-
+import {View} from 'react-native';
+import {
+  Container,
+  Button,
+  Text,
+  H1,
+  Input,
+  Form,
+  Item,
+  Toast,
+} from 'native-base';
 const Login = () => {
-  return <Text>Login</Text>;
+  return (
+    <Container>
+      <View>
+        <H1>UpTask</H1>
+        <Form>
+          <Item inlineLabel last>
+            <Input autoCompleteType="email" placeholder="Email" />
+          </Item>
+          <Item inlineLabel last>
+            <Input secureTextEntry={true} placeholder="Password" />
+          </Item>
+        </Form>
+        <Button>
+          <Text>Iniciar Sesión</Text>
+        </Button>
+        <Text>Crear Cuenta</Text>
+      </View>
+    </Container>
+  );
 };
 
 export default Login;
