7e1f0c03862d0084e9ea88c566f4cf8fc6fc56a4
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-09-24 20:15:12 -0600

    validating the create account form

--- App.js
+++ App.js
@@ -4,39 +4,41 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
-
+import {Root} from 'native-base';
 import Login from './views/Login';
 import NewAccount from './views/NewAccount';
 
 const App = () => {
   return (
     <>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Login">
-          <Stack.Screen
-            name="Login"
-            component={Login}
-            options={{
-              title: 'Iniciar Sesión',
-              headerShown: false,
-            }}
-          />
-          <Stack.Screen
-            name="NewAccount"
-            component={NewAccount}
-            options={{
-              title: 'Crear cuenta',
-              headerStyle: {
-                backgroundColor: '#28303B',
-              },
-              headerTintColor: '#fff',
-              headerTitleStyle: {
-                fontWeight: 'bold',
-              },
-            }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <Root>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Login">
+            <Stack.Screen
+              name="Login"
+              component={Login}
+              options={{
+                title: 'Inciar Sesión',
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="NewAccount"
+              component={NewAccount}
+              options={{
+                title: 'Crear cuenta',
+                headerStyle: {
+                  backgroundColor: '#28303B',
+                },
+                headerTintColor: '#FFF',
+                headerTitleStyle: {
+                  fontWeight: 'bold',
+                },
+              }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Root>
     </>
   );
 };
--- views/NewAccount.js
+++ views/NewAccount.js
@@ -12,24 +12,70 @@ import {
   Toast,
 } from 'native-base';
 const NewAccount = () => {
+  // state the form
+  const [name, saveName] = useState('');
+  const [email, saveEmail] = useState('');
+  const [password, savePassword] = useState('');
+
+  const [message, saveMessage] = useState(null);
+
+  // where the user press in new account
+  const handleSubmit = () => {
+    // validate
+    if (name === '' || email === '' || password === '') {
+      //  show a error
+      saveMessage('Todos los campos son obligatorios');
+      return;
+    }
+    // password at least 6 characters
+    if (password.length < 6) {
+      saveMessage('El password debe ser al menos 6 carácteres');
+      return;
+    }
+    // save the user
+  };
+
+  const showAlert = () => {
+    Toast.show({
+      text: message,
+      buttonText: 'OK',
+      duration: 5000,
+    });
+  };
+
   return (
     <Container style={[globalStyles.Container, {backgroundColor: '#E84347'}]}>
       <View style={globalStyles.content}>
         <H1 style={globalStyles.title}>UpTask</H1>
         <Form>
           <Item inlineLabel last style={globalStyles.input}>
-            <Input autoCompleteType="name" placeholder="Nombre" />
+            <Input
+              placeholder="Nombre"
+              onChangeText={(text) => saveName(text)}
+            />
           </Item>
           <Item inlineLabel last style={globalStyles.input}>
-            <Input autoCompleteType="email" placeholder="Email" />
+            <Input
+              placeholder="Email"
+              onChangeText={(text) => saveEmail(text)}
+            />
           </Item>
           <Item inlineLabel last style={globalStyles.input}>
-            <Input secureTextEntry={true} placeholder="Password" />
+            <Input
+              secureTextEntry={true}
+              placeholder="Password"
+              onChangeText={(text) => savePassword(text)}
+            />
           </Item>
         </Form>
-        <Button square block style={globalStyles.button}>
+        <Button
+          square
+          block
+          style={globalStyles.button}
+          onPress={() => handleSubmit()}>
           <Text style={globalStyles.buttonText}>Crear cuenta</Text>
         </Button>
+        {message && showAlert()}
       </View>
     </Container>
   );
