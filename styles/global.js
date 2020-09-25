65cf24938ab8edd1faf19d8ffda59e2ba456c52a
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-09-24 19:26:07 -0600

    Adding styles to the authentication form

---
+++ styles/global.js
@@ -0,0 +1,41 @@
+import {StyleSheet} from 'react-native';
+
+const globalStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  content: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    marginHorizontal: '2.5%',
+    flex: 1,
+  },
+  title: {
+    textAlign: 'center',
+    marginBottom: 20,
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#FFF',
+  },
+  input: {
+    backgroundColor: '#FFF',
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#28303B',
+  },
+  buttonText: {
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
+    color: '#FFF',
+  },
+  link: {
+    color: '#FFF',
+    marginTop: 60,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 18,
+    textTransform: 'uppercase',
+  },
+});
+export default globalStyles;
