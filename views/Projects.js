95a1c96c45e7028d77d2ccfd940ec38675efe5f6
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-09-25 18:37:15 -0600

    Creating project screen

--- views/Projects.js
+++ views/Projects.js
@@ -1,7 +1,27 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {View} from 'react-native';
+import {
+  Container,
+  Button,
+  Text,
+  H2,
+  Content,
+  List,
+  ListItem,
+  Left,
+  Right,
+} from 'native-base';
+
+import globalStyles from '../styles/global';
 const Projects = () => {
-  return <Text>Proyectos</Text>;
+  return (
+    <Container style={([globalStyles.container], {backgroundColor: '#E84347'})}>
+      <Button style={[globalStyles.button, {marginTop: 30}]} square block>
+        <Text style={globalStyles.buttonText}>Nuevo Proyecto</Text>
+      </Button>
+      <H2 style={globalStyles.subtitle}>Selecciona un project</H2>
+    </Container>
+  );
 };
 
 export default Projects;
