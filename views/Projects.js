99d892e042f375e780b2c63af53ede72a14c0734
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-09-25 19:44:17 -0600

    Creating the form to create new projects

--- views/Projects.js
+++ views/Projects.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {View} from 'react-native';
 import {
   Container,
   Button,
@@ -11,12 +10,18 @@ import {
   Left,
   Right,
 } from 'native-base';
-
+import {useNavigation} from '@react-navigation/native';
 import globalStyles from '../styles/global';
 const Projects = () => {
+  // React navigation
+  const navigation = useNavigation();
   return (
     <Container style={([globalStyles.container], {backgroundColor: '#E84347'})}>
-      <Button style={[globalStyles.button, {marginTop: 30}]} square block>
+      <Button
+        style={[globalStyles.button, {marginTop: 30}]}
+        square
+        block
+        onPress={() => navigation.navigate('NewProjects')}>
         <Text style={globalStyles.buttonText}>Nuevo Proyecto</Text>
       </Button>
       <H2 style={globalStyles.subtitle}>Selecciona un project</H2>
