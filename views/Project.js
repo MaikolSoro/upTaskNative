a8c4398f4bca713b2051a375a3d92da962670fc7
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-09-29 19:47:44 -0600

    Forms to create tasks

--- views/Project.js
+++ views/Project.js
@@ -1,7 +1,31 @@
 import React from 'react';
-import {Text} from 'native-base';
+import {
+  Container,
+  Button,
+  Text,
+  H2,
+  Form,
+  Item,
+  Input,
+  List,
+  Toast,
+} from 'native-base';
+
+import globalStyles from '../styles/global';
+
 const Project = ({route}) => {
-  return <Text>Hola</Text>;
+  return (
+    <Container style={([globalStyles.Container], {backgroundColor: '#e84347'})}>
+      <Form style={{marginHorizontal: '2.5%', marginTop: 20}}>
+        <Item inlineLabel last style={globalStyles.input}>
+          <Input placeholder="Nombre Tarea" />
+        </Item>
+        <Button style={globalStyles.button} square block>
+          <Text>Crear Tarea</Text>
+        </Button>
+      </Form>
+    </Container>
+  );
 };
 
 export default Project;
