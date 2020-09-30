aba246b9c773c07c91e8978f49ff256b8dad7386
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-09-29 19:06:35 -0600

    Update projects when new ones are added

--- views/NewProject.js
+++ views/NewProject.js
@@ -12,21 +12,65 @@ import {
 } from 'native-base';
 import globalStyles from '../styles/global';
 import {useNavigation} from '@react-navigation/native';
+import {gql, useMutation} from '@apollo/client';
 
+const NEW_PROJECT = gql`
+  mutation newProject($input: ProjectInput) {
+    newProject(input: $input) {
+      name
+      id
+    }
+  }
+`;
+// Actualizar el cache
+const GET_PROJECTS = gql`
+  query getProjects {
+    getProjects {
+      id
+      name
+    }
+  }
+`;
 const NewProject = () => {
   // React navigation
   const navigation = useNavigation();
+
   // state del component
   const [name, saveName] = useState('');
   const [message, saveMessage] = useState(null);
 
+  // mutation apollo
+  const [newProject] = useMutation(NEW_PROJECT, {
+    update(cache, {data: {newProject}}) {
+      const {getProjects} = cache.readQuery({query: GET_PROJECTS});
+      cache.writeQuery({
+        query: GET_PROJECTS,
+        data: {getProjects: getProjects.concat([newProject])},
+      });
+    },
+  });
+
   // validate create project
-  const handleSubmit = () => {
+  const handleSubmit = async () => {
     if (name === '') {
       saveMessage('El nombre del proyecto es obligatorio');
       return;
     }
     // save the project in the BD
+
+    try {
+      const {data} = await newProject({
+        variables: {
+          input: {
+            name,
+          },
+        },
+      });
+      saveMessage('Proyecto creado correctamente');
+      navigation.navigate('Projects');
+    } catch (error) {
+      saveMessage(error.message.replace('GraphQL error:', ''));
+    }
   };
   const showAlert = () => {
     Toast.show({
@@ -52,7 +96,7 @@ const NewProject = () => {
           square
           block
           onPress={() => handleSubmit()}>
-          <Text style={globalStyles.buttonText}>Crear Proyecto</Text>
+          <Text style={globalStyles.buttonText}>Nuevo Proyecto</Text>
         </Button>
         {message && showAlert()}
       </View>
